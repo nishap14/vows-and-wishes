5936cd445fcbc537e1c3395fd6ec445591981eeb
Nisha Parchande <nishaparchande14@gmail.com>
2025-11-25 11:41:14 +0530

    point frontend to deployed backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = "https://vows-and-wishes-gu4k.vercel.app/api";
+// frontend/src/config.js
+const API_BASE_URL = "https://vows-and-wishes.vercel.app/api";
 export default API_BASE_URL;
